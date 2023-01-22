4abd7bead08ec2733a177b0f5e8569d17b5e753d
NoorulAmeenJ <noorularafat786@gmail.com>
2023-01-22 12:29:05 +0530

    day-1 completed

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data = [
+    {
+      name: "Noorul",
+      company: "Profinch"
+    },
+    {
+      name: "Shanmu",
+      company: "Wipro"
+    },
+    {
+      name: "Nirmal",
+      company: "Ashok"
+    }
+  ]
   return ( 
     <div className="App">
-        <SampleCard/>
-        <SampleCard/>
-        <SampleCard/>
+      {
+        data.map((item,id)=>(
+          <SampleCard key={id} name={item.name} company={item.company} />
+        ))
+      }
 
        </div>
   );
 }
-
 export default App;
-
-
 // for function small 1st letter 
 // for component capital 1st letter 
- function SampleCard() {
+ function SampleCard(props) {
+ 
   return(
-    <div>
-      <h1>Cards</h1>
-      <h1>Cards-details</h1>
+    <div className="box" style={{backgroundColor:props.clr}} >
+      <p>Cards : {props.name}</p>
+      <p>Cards-details : {props.company}</p>
     </div>
   )
 }
